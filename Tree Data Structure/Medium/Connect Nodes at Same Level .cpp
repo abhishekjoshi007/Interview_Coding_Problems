62035cc5a45c32def97f75170bc6a5f98c7726c2
Abhishek Joshi <abhishekjoshi.ddun@gmail.com>
2020-09-30 19:41:35 +0530

    Create Connect Nodes at Same Level .cpp

---
+++ Tree Data Structure/Medium/Connect Nodes at Same Level .cpp
@@ -0,0 +1,96 @@
+/*Given a binary tree, connect the nodes that are at same level. You'll be given an addition nextRight pointer for the same.
+
+Initially, all the nextRight pointers point to garbage values. Your function should set these pointers to point next right for each node.
+       10                       10 ------> NULL
+      / \                       /      \
+     3   5       =>     3 ------> 5 --------> NULL
+    / \     \               /  \           \
+   4   1   2          4 --> 1 -----> 2 -------> NULL
+
+ 
+
+Example 1:
+
+Input:
+     3
+   /  \
+  1    2
+Output:
+3 1 2
+1 3 2
+Explanation:The connected tree is
+        3 ------> NULL
+     /    \
+    1-----> 2 ------ NULL
+Example 2:
+
+Input:
+      10
+    /   \
+   20   30
+  /  \
+ 40  60
+Output:
+10 20 30 40 60
+40 20 60 10 30
+Explanation:The connected tree is
+         10 ----------> NULL
+       /     \
+     20 ------> 30 -------> NULL
+  /    \
+ 40 ----> 60 ----------> NULL
+Your Task:
+You don't have to take input. Complete the function connect() that takes root as parameter and connects the nodes at same level. The printing is done by the driver code.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(N).
+
+Constraints:
+1 <= Number of nodes <= 100
+1 <= Data of a node <= 1000
+
+Question Link:https://practice.geeksforgeeks.org/problems/connect-nodes-at-same-level/1
+*/
+
+// Should set the nextRight for all nodes
+void connect(Node *p)
+{
+   if(p==NULL)
+   return;
+   
+   //Doing level order Traversal
+   queue<Node *> q;
+   q.push(p);
+   
+   while(!q.empty())
+   { 
+       int count=q.size();
+       vector<Node* > temp;
+       
+       while(count--)
+       {
+           Node* node=q.front();
+           q.pop();
+           
+           temp.push_back(node);
+           
+           if(node->left)
+           q.push(node->left);
+           
+           if(node->right)
+           q.push(node->right);
+           
+           
+       }
+       
+       for(int i=0;i<temp.size();i++)
+       {
+           if(i==temp.size()-1)
+           temp[i]->nextRight=NULL;
+           
+           else 
+           temp[i]->nextRight=temp[i+1];
+       }
+   }
+}
+

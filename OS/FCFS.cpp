10cc452a2598d02449043718767ca4cdb52137f6
Abhishek Joshi <32777573+abhishekjoshi007@users.noreply.github.com>
2023-09-27 01:13:02 +0530

    SSTF using vector array

--- OS/FCFS.cpp
+++ OS/FCFS.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
   using namespace std;
-  void FCFS(int arr[], int head, int size)
+  void FCFS(queue<int> arr, int head, int size)
   {
+    //Seek_count to calculate total number of seek operations
     int seek_count=0;
+    //distance to calculate the distance between head and the current disk
     int distance, curr_track;
 
     for(int i=0;i<size;i++)
@@ -11,27 +13,37 @@
 
         distance=abs(curr_track-head);
         seek_count+=distance;
-
         head=curr_track;
     }
 
     
-        cout<<seek_count<<endl;
+        cout<<"Total number of seek operations="<<seek_count<<endl;
   
   }
   int main()
   {
+    //initilising a queue
+    queue <int> sstf;
+    //defining size /you can use any size of your choice
     int size=50;
-    int arr[size];
+    //int arr[size];
+    queue<int> arr;
     //Randomly generating 50 elements in the array 
     for(int i=0;i<size;i++)
     {
-        arr[i]=rand()%100;
+        arr.push(rand()%100);
     }
+    while (!arr.empty()) {
+         cout<<arr.front();
+        arr.pop();
+    }
+    
 
-    //initilising Head 
+
+    //initilising Head as 50 
     int head=50;
-
+    
+    //Calling function FCFS
     FCFS(arr, head, size);
     
     return 0;
--- OS/SSTF.cpp
+++ OS/SSTF.cpp
@@ -3,3 +3,61 @@ next location to move the disk head. Randomly generate these numbers. Place 50 o
 let the algorithm run. Finally, as the output let it print the total movement in the number of cylinders. 
 Properly comment/document your code.*/
 
+#include <bits/stdc++.h>
+using namespace std;
+    
+    // Function to calculate absolute seek time
+    int calSeekTime(int currentPos,int nextPos)
+    {
+        return abs(nextPos-currentPos);
+    }
+    
+  int main()
+  {
+    int size=50;
+
+    //dynamic vector of size 50
+    vector<int> arr(size);
+
+    //Randomly putting up element in the vector array of size 50 
+    for(int i=0;i<arr.size();i++)
+    {
+        arr[i]=rand()%100;
+    }
+    
+    int totalCount=0,  currentPosition = 60; // Starting position (you can change this as needed)
+    while(!arr.empty())
+    {
+        int minseektime=INT_MAX;
+        int nearestPointer=-1;
+        // Find the nearest request
+        for (int i = 0; i < arr.size(); ++i) {
+            int request = arr[i];
+            int seekTime=calSeekTime(currentPosition,request);
+            if(seekTime<minseektime)
+            {
+                minseektime=seekTime;
+                nearestPointer=i;
+            }
+            }
+        
+        // Calculate seek time to the nearest request
+        totalCount += minseektime;
+
+        // Update the current position
+        currentPosition = arr[nearestPointer];
+
+        // Remove the processed request from the vector 
+        arr.erase(arr.begin() + nearestPointer);
+      
+      cout << "Disk head moved to cylinder " << currentPosition << ", Total Movement: " << totalCount << " cylinders" << endl;
+    }
+
+    
+
+    
+    cout << "Total movement of the disk head: " << totalCount << " cylinders" << endl;  
+            
+    return 0;
+  
+  }

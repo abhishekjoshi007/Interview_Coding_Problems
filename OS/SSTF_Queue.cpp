b04de5feaf47d004ce2095b50a44b57f774c32b0
Abhishek Joshi <32777573+abhishekjoshi007@users.noreply.github.com>
2023-09-29 10:17:37 +0530

    Problem Statement Added

--- OS/SSTF_Queue.cpp
+++ OS/SSTF_Queue.cpp
@@ -1,3 +1,8 @@
+/*Using a programming language of your choice, implement the SSTF algorithm. You will use a queue to store the 
+next location to move the disk head. Randomly generate these numbers. Place 50 of them in the queue and then 
+let the algorithm run. Finally, as the output let it print the total movement in the number of cylinders. 
+Properly comment/document your code.*/
+
 #include <bits/stdc++.h>
 using namespace std;
 

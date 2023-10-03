f1c823421568a6f7572d0cc906d21fb707be60ed
Abhishek Joshi <32777573+abhishekjoshi007@users.noreply.github.com>
2023-10-04 04:20:38 +0530

    longest common prefix

--- Strings/Easy/Longest Common Prefix.cpp
+++ Strings/Easy/Longest Common Prefix.cpp
@@ -1,17 +1,15 @@
-/*
-Ques - https://leetcode.com/problems/longest-palindromic-substring/description/
+/*/https://leetcode.com/problems/longest-palindromic-substring/description/
 
-Algorithm
+Algorithm 
 
-Follow the steps mentioned below to implement the idea:
-
-- Use two pointers, **low** and **hi**, for the left and right end of the current palindromic substring being found.
-- Then checks if the characters at **str[low]** and **str[hi]** are the same.
-    - If they are, it expands the substring to the left and right by decrementing **low** and incrementing **hi**.
-    - It continues this process until the characters at **str[low]** and **str[hi]** are unequal or until the indices are in bounds.
-- If the length of the current palindromic substring becomes greater than the maximum length, it updates the maximum length.
-
-*/
+1. Initialize an empty string ans to store the common prefix.
+2. Sort the input list v lexicographically. This step is necessary because the common prefix should be common to all the strings, so we need to find the common prefix of the first and last string in the sorted list.
+This in most of the sorting 
+coz elements of vector are in lexicographic order and we are cheking only the first and the last element to check the common words 
+3. Iterate through the characters of the first and last string in the sorted list, stopping at the length of the shorter string.
+4. If the current character of the first string is not equal to the current character of the last string, return the common prefix found so far.
+5. Otherwise, append the current character to the ans string.
+6. Return the ans string containing the longest common prefix.*/
 
 class Solution {
 public:

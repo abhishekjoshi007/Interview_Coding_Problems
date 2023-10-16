02729340b5f8993665d3c09bf2fb9d6db02d762e
Abhishek <abhishekjoshi.ddun@gmail.com>
2023-10-16 13:50:03 -0500

    Subset Sum

---
+++ Recursion/Medium/subset_sum.cpp
@@ -0,0 +1,31 @@
+/*https://practice.geeksforgeeks.org/problems/subset-sums2234/1*/
+class solution {
+public:
+    void ss(vector<int> arr, vector<int> &ans, int i, int sum)
+    {
+        if(i==arr.size())
+        {   
+            
+            ans.push_back(sum);
+            return;
+        }
+        
+        //include 
+        ss(arr,ans,i+1,sum+arr[i]);
+        
+        //exclude 
+        ss(arr,ans,i+1, sum);
+        
+    }
+
+    vector<int> subsetSums(vector<int> arr, int N)
+    {
+        vector<int >ans;
+       
+        int i=0,sum=0;
+        
+        ss(arr,ans,i,sum);
+        return ans;
+        
+    }
+}

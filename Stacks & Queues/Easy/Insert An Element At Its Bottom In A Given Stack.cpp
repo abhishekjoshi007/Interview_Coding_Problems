8813874918017573c3fc34b9672c66f5ad660607
Abhishek Joshi <32777573+abhishekjoshi007@users.noreply.github.com>
2024-10-21 13:45:30 -0500

    Insert element at bottom of stack

---
+++ Stacks & Queues/Easy/Insert An Element At Its Bottom In A Given Stack.cpp
@@ -0,0 +1,24 @@
+/*https://www.naukri.com/code360/problems/insert-an-element-at-its-bottom-in-a-given-stack_1171166?topList=love-babbar-dsa-sheet-problems&leftPanelTab=0%3Fsource%3Dyoutube&campaign=Lovebabbarcodestudio&utm_source=youtube&utm_medium=affiliate&utm_campaign=Lovebabbarcodestudio&leftPanelTabValue=PROBLEM*/
+
+
+#include <bits/stdc++.h>
+
+void solve(stack<int>& myStack, int x) {
+
+  if (myStack.empty()) {
+    myStack.push(x);
+    return;
+  }
+
+  int num=myStack.top();
+  myStack.pop();
+
+  solve(myStack,x);
+  myStack.push(num);
+
+}
+stack<int> pushAtBottom(stack<int>& myStack, int x) 
+{
+    solve(myStack,x);
+    return myStack;
+}
